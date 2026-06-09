70a061dc4aa68285f9d9eadfd464459e9745dd54
lirneis <olga05092000@gmail.com>
2026-06-09 23:20:51 +0300

    lesson_1.1: add allure

--- src/test/java/TestPet.java
+++ src/test/java/TestPet.java
@@ -1,8 +1,10 @@
-import io.qameta.allure.TmsLink;
+import io.qameta.allure.*;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static io.qameta.allure.Allure.step;
 import static io.restassured.RestAssured.given;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -10,22 +12,32 @@ public class TestPet {
 
     @Test
     @TmsLink("https://team-w1q0.testit.software/projects/1/tests/39")
+    @Feature("Pet")
+    @Severity(SeverityLevel.NORMAL)
+    @Owner("lirneis")
+    @DisplayName("Попытка удалить несуществующего питомца (DELETE /pet/{petId})")
     public void testDeleteNonExistentPet() {
         Response response = given()
                 .contentType(ContentType.JSON)
-                .header("Accept","application/json")
+                .header("Accept", "application/json")
                 .when()
-                .delete(Base.BASE_URL +"/pet/9999");
+                .delete(Base.BASE_URL + "/pet/9999");
 
         final String responseBody = response.getBody().asString();
 
-        assertEquals(200,response.getStatusCode(),
-                "Response code does not equal the expected result. Actual result:" + responseBody);
-        assertEquals("Pet deleted", responseBody,
-                "Error text does not match the expected response. Actual text: " + responseBody);
+        step("Check response code is 200", () ->
+                assertEquals(200, response.getStatusCode(),
+                        "Response code does not equal the expected result. " +
+                                "Actual result:" + responseBody)
+        );
+        step("Check message is \"Pet deleted\"", () ->
+                assertEquals("Pet deleted", responseBody,
+                        "Error text does not match the expected response." +
+                                "Actual text: " + responseBody)
+        );
     }
 
-public enum Base {
+    public enum Base {
         ;
         public static final String BASE_URL = "http://5.181.109.28:9090/api/v3";
     }
