9f1622db4db3a32946b10bf61f16e7d116cd594a
lirneis <olga05092000@gmail.com>
2026-06-12 00:38:48 +0300

    lesson_3: add parametrized test testAddNewPet

--- src/test/java/tests/TestPet.java
+++ src/test/java/tests/TestPet.java
@@ -6,6 +6,8 @@ import io.restassured.response.Response;
 import models.Pet;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static io.qameta.allure.Allure.step;
 import static io.restassured.RestAssured.given;
@@ -57,7 +59,7 @@ public class TestPet {
                 .header("Accept", "application/json")
                 .body(pet)
                 .when()
-                .put(Base.BASE_URL + "/pet/");
+                .put(Base.BASE_URL + "/pet");
 
         final String responseBody = response.getBody().asString();
 
@@ -72,6 +74,49 @@ public class TestPet {
                                 "Actual text: " + responseBody)
         );
 
+    }
+
+    @ParameterizedTest
+    @TmsLink("https://team-w1q0.testit.software/projects/1/tests/31")
+    @CsvSource({
+            "207, Ace, available",
+            "208, Bertie, pending",
+            "209, Yum, sold"
+    })
+    @Feature("Pet")
+    @Severity(SeverityLevel.CRITICAL)
+    @Owner("lirneis")
+    @DisplayName("Добавление нового питомца (POST /pet)")
+    public void testAddNewPet(int id, String name, String status) {
+        Pet pet = new Pet();
+        pet.setId(id);
+        pet.setName(name);
+        pet.setStatus(status);
+
+        Response response = given()
+                .contentType(ContentType.JSON)
+                .header("Accept", "application/json")
+                .body(pet)
+                .when()
+                .post(Base.BASE_URL + "/pet");
+
+        String responseBody = response.getBody().asString();
+
+        step("Check response code is 200", () ->
+                assertEquals(200, response.getStatusCode(),
+                        "Response code does not equal the expected result. " + responseBody)
+        );
+
+        step("Check parameters of the created pet", () -> {
+                    Pet createdPet = response.as(Pet.class);
+                    assertEquals(pet.getId(), createdPet.getId(),
+                            "pet id does not match the expected result");
+                    assertEquals(pet.getName(), createdPet.getName(),
+                            "pet name does not match the expected result");
+                    assertEquals(pet.getStatus(), createdPet.getStatus(),
+                            "pet status does not match the expected result");
+                }
+        );
     }
 
     public enum Base {
